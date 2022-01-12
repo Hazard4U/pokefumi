8394ef4b4bf40dd8f5b2b8e6c06eb03c3be51cfc
Louis Muzellec <louis.muzellec@gmail.com>
2022-01-12 09:56:52 +0100

    User data

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
+
+import users from './users.json';
+
 const app = express();
 app.use(bodyParser.json({
     limit: '50mb',
@@ -7,6 +10,12 @@ app.use(bodyParser.json({
         req.rawBody = buf;
     }
 }));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/user', (req,res) => {
+    res.status(200).json(users)
+})
+
 export {app};
 
