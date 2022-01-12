cab23da16ab4a0c17cbcb2d97cbe34aefcc71d34
Louis Muzellec <louis.muzellec@gmail.com>
2022-01-12 10:22:39 +0100

    User routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
 
-import users from './users.json';
-import { findUser } from './userController'
+import { User } from './model'
+import { listUsers, findUser, addUser } from './userController'
 
 const app = express();
 app.use(bodyParser.json({
@@ -15,13 +15,19 @@ app.use(bodyParser.json({
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/user', (req,res) => {
-    res.status(200).json(users)
+    res.status(200).json(listUsers())
+})
+
+app.post('/user', (req, res) => {
+    const newUser: User = req.body;
+    res.status(200).json(addUser(newUser))
 })
 
 app.get('/user/:id', (req, res) => {
     const userId: number = parseFloat(req.params.id)
     res.status(200).json(findUser(userId))
 })
+
 
 export {app};
 
