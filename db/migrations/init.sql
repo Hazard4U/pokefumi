16d497b132cb5acebbde4c241e8f8b46d7991b1c
Théo Martel <martel.theo@outlook.com>
2022-01-12 11:23:06 +0100

    fix: sql syntax error

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
   user_id INTEGER PRIMARY KEY,
   name	TEXT NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS matchs (
   match_id INTEGER PRIMARY KEY,
-  user_id_1 INTEGER,
+  user_id_1 INTEGER NOT NULL,
   user_id_2 INTEGER,
   pokemon_user_1 TEXT,
   pokemon_user_2 TEXT,
@@ -13,4 +13,4 @@ CREATE TABLE IF NOT EXISTS matchs (
   score_user_2   INTEGER DEFAULT 0,
   FOREIGN KEY(user_id_1) REFERENCES users(user_id),
   FOREIGN KEY(user_id_2) REFERENCES users(user_id)
-)
+);
