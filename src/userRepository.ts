13b86a05119fac24c7b5482625bcf6359fc92d15
Théo Martel <martel.theo@outlook.com>
2022-01-12 10:15:36 +0100

    feat: add part of CRUD operations  for User

--- src/userRepository.ts
+++ src/userRepository.ts
@@ -1,29 +1,48 @@
-import Database from 'better-sqlite3'
-import fs from 'fs'
+import Database from "better-sqlite3";
+import fs from "fs";
 import migrations from "../db/migrations/migrations.json";
+import { User } from "./model";
 
 export default class UserRepository {
-    db: Database.Database
-  
-    
-  
-    constructor() {
-      this.db = new Database('db/users.db', { verbose: console.log });
-      this.applyMigrations()    
-    }
-  
-    applyMigrations(){
-      const applyMigration = (path: string) => {
-        const migration = fs.readFileSync(path, 'utf8')
-        this.db.exec(migration)
-      }
-      
-      const testRow = this.db.prepare("SELECT name FROM sqlite_schema WHERE type = 'table' AND name = 'users'").get()
-  
-      if (!testRow){
-        console.log('Applying migrations on DB users...')
-        migrations.forEach(applyMigration)
-      }
+  db: Database.Database;
+
+  constructor() {
+    this.db = new Database("db/users.db", { verbose: console.log });
+    this.applyMigrations();
+  }
+
+  applyMigrations() {
+    const applyMigration = (path: string) => {
+      const migration = fs.readFileSync(path, "utf8");
+      this.db.exec(migration);
+    };
+
+    const testRow = this.db
+      .prepare(
+        "SELECT name FROM sqlite_schema WHERE type = 'table' AND name = 'users'"
+      )
+      .get();
+
+    if (!testRow) {
+      console.log("Applying migrations on DB users...");
+      migrations.forEach(applyMigration);
     }
   }
-  
+
+  getAllUsers(): User[] {
+    const statement = this.db.prepare("SELECT * FROM users");
+    const rows: User[] = statement.all();
+    return rows;
+  }
+
+  getUserById(userId: number) {
+    const statement = this.db.prepare("SELECT * FROM users WHERE user_id = ?");
+    const rows: User[] = statement.get(userId);
+    return rows;
+  }
+
+  createUser(name: string) {
+    const statement = this.db.prepare("INSERT INTO users (name) VALUES (?)");
+    return statement.run(name).lastInsertRowid;
+  }
+}
