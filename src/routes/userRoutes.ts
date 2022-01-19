da3820acac08d467ec4bed1a1351ddc308ee6d22
Théo Martel <martel.theo@outlook.com>
2022-01-19 12:11:39 +0100

    chore: use static methods for controllers, services

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,15 +5,15 @@ import { User } from "../models/User";
 const userController = new UserController();
 export const userRoutes = express.Router();
 userRoutes.route("/").get((req, res) => {
-  res.status(200).json(userController.listUsers());
+  res.status(200).json(UserController.listUsers());
 });
 
 userRoutes.route("/:id").get((req, res) => {
   const userId: number = parseFloat(req.params.id);
-  res.status(200).json(userController.findUser(userId));
+  res.status(200).json(UserController.findUser(userId));
 });
 
 userRoutes.route("/").post((req, res) => {
   const { name } : { name: string } = req.body;
-  res.status(200).json(userController.addUser(name));
+  res.status(200).json(UserController.addUser(name));
 });
