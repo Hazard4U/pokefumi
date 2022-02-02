5f204730b5ba8ed5179d92f7661929f677f7fae5
Théo Martel <martel.theo@outlook.com>
2022-02-02 13:49:51 +0100

    chore: docker compose

--- match-service/src/routes/routes.ts
+++ match-service/src/routes/routes.ts
@@ -1,5 +1,4 @@
 import * as express from "express"
-import * as path from "path"
 import { userRoutes } from './userRoutes'
 import { matchRoutes } from './matchRoutes'
 import { pokemonRoutes } from "./pokemonRoutes"
@@ -7,7 +6,7 @@ import { roundRoutes } from './roundRoutes'
 
 export const router = ( app: express.Application ) => {
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname + '/../login.html'))
+        res.send("Match-Service")
     });
 
     app.use('/user', userRoutes);
